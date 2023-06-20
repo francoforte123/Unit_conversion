d4752fd7912cd5708f477f159964e85eb46188fa
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-06-20 22:45:39 +0200

    Aggiungere i file di progetto.

---
+++ Unit_conversion/Conversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_conversion
+{
+    internal class Conversion{
+
+        public void conversionCelsusToFahrenheit(int value) {
+            double result = (value * 1.8) + 32;
+            Console.WriteLine("the result of conversion is= " + result);
+        }
+    }
+}
---
+++ Unit_conversion/Program.cs
@@ -0,0 +1,16 @@
+﻿using Unit_conversion;
+
+Conversion conversion= new Conversion();
+
+Console.Write("inserisci un valore di gradi da Celsus a Fahrenheit= ");
+int grade= int.Parse(Console.ReadLine());
+
+conversion.conversionCelsusToFahrenheit(grade);
+
+
+
+
+
+
+
+
